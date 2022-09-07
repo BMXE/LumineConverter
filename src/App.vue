47d608aba0705c6ef0449038f33f8e5e5e9c434b
Semih Kaiser <git@oryfox.de>
2022-09-07 17:39:44 +0200

    Profile icon

--- src/App.vue
+++ src/App.vue
@@ -1,20 +1,69 @@
 <template>
     <div class="root" v-if="access_token">
         <div class="h-stack">
-            <input type="text" placeholder="URL to Apple Music Playlist" ref="url">
-            <div class="button-base" @click="fetchAndParsePlaylist">Import playlist</div>
-            <div class="button-base" @click="postDataToSpotify" :disabled="posting">Save</div>
-            <input type="file" ref="imageSelector" @change="selectedButton">
-            <img :src="imageBuffer" alt="" @click="this.$refs.imageSelector.click()" v-if="imageBuffer">
-            <div class="button-base" @click="this.$refs.imageSelector.click()" v-else>Select playlist image</div>
-            <div class="button-base" @click="getRandomImage">Random</div>
+            <div class="user-button" v-if="user" @click="openUserProfile">
+                <img class="user-image" :src="user.images[0].url" v-if="user.images[0].url" />
+                <svg xmlns="http://www.w3.org/2000/svg" width="45" height="45" fill="currentColor" viewBox="0 0 16 16"
+                    v-else>
+                    <path d="M11 6a3 3 0 1 1-6 0 3 3 0 0 1 6 0z" />
+                    <path fill-rule="evenodd"
+                        d="M0 8a8 8 0 1 1 16 0A8 8 0 0 1 0 8zm8-7a7 7 0 0 0-5.468 11.37C3.242 11.226 4.805 10 8 10s4.757 1.225 5.468 2.37A7 7 0 0 0 8 1z" />
+                </svg>
+                <span class="user-name">{{ user.display_name }}</span>
+            </div>
+            <input type="text" class="url-input" placeholder="URL to Apple Music Playlist" ref="url">
+            <div class="button-base" @click="fetchAndParsePlaylist">
+                <svg xmlns="http://www.w3.org/2000/svg" width="32" height="32" fill="currentColor" viewBox="0 0 16 16">
+                    <path
+                        d="M4.406 1.342A5.53 5.53 0 0 1 8 0c2.69 0 4.923 2 5.166 4.579C14.758 4.804 16 6.137 16 7.773 16 9.569 14.502 11 12.687 11H10a.5.5 0 0 1 0-1h2.688C13.979 10 15 8.988 15 7.773c0-1.216-1.02-2.228-2.313-2.228h-.5v-.5C12.188 2.825 10.328 1 8 1a4.53 4.53 0 0 0-2.941 1.1c-.757.652-1.153 1.438-1.153 2.055v.448l-.445.049C2.064 4.805 1 5.952 1 7.318 1 8.785 2.23 10 3.781 10H6a.5.5 0 0 1 0 1H3.781C1.708 11 0 9.366 0 7.318c0-1.763 1.266-3.223 2.942-3.593.143-.863.698-1.723 1.464-2.383z" />
+                    <path
+                        d="M7.646 15.854a.5.5 0 0 0 .708 0l3-3a.5.5 0 0 0-.708-.708L8.5 14.293V5.5a.5.5 0 0 0-1 0v8.793l-2.146-2.147a.5.5 0 0 0-.708.708l3 3z" />
+                </svg>
+            </div>
+            <div class="button-base" @click="postDataToSpotify" :disabled="posting">
+                <svg xmlns="http://www.w3.org/2000/svg" width="32" height="32" fill="currentColor" viewBox="0 0 16 16">
+                    <path fill-rule="evenodd"
+                        d="M4.406 1.342A5.53 5.53 0 0 1 8 0c2.69 0 4.923 2 5.166 4.579C14.758 4.804 16 6.137 16 7.773 16 9.569 14.502 11 12.687 11H10a.5.5 0 0 1 0-1h2.688C13.979 10 15 8.988 15 7.773c0-1.216-1.02-2.228-2.313-2.228h-.5v-.5C12.188 2.825 10.328 1 8 1a4.53 4.53 0 0 0-2.941 1.1c-.757.652-1.153 1.438-1.153 2.055v.448l-.445.049C2.064 4.805 1 5.952 1 7.318 1 8.785 2.23 10 3.781 10H6a.5.5 0 0 1 0 1H3.781C1.708 11 0 9.366 0 7.318c0-1.763 1.266-3.223 2.942-3.593.143-.863.698-1.723 1.464-2.383z" />
+                    <path fill-rule="evenodd"
+                        d="M7.646 4.146a.5.5 0 0 1 .708 0l3 3a.5.5 0 0 1-.708.708L8.5 5.707V14.5a.5.5 0 0 1-1 0V5.707L5.354 7.854a.5.5 0 1 1-.708-.708l3-3z" />
+                </svg>
+            </div>
+            <div class="button-base" @click="toggleImageModal">
+                <svg xmlns="http://www.w3.org/2000/svg" width="32" height="32" fill="currentColor" viewBox="0 0 16 16">
+                    <path d="M6.002 5.5a1.5 1.5 0 1 1-3 0 1.5 1.5 0 0 1 3 0z" />
+                    <path
+                        d="M2.002 1a2 2 0 0 0-2 2v10a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2V3a2 2 0 0 0-2-2h-12zm12 1a1 1 0 0 1 1 1v6.5l-3.777-1.947a.5.5 0 0 0-.577.093l-3.71 3.71-2.66-1.772a.5.5 0 0 0-.63.062L1.002 12V3a1 1 0 0 1 1-1h12z" />
+                </svg>
+            </div>
         </div>
         <span v-if="fetching">Retrieving and parsing data</span>
-        <div class="content-pane">
+        <div class="content-pane" v-if="playlist && playlist.title !== null && playlist.title !== undefined">
             <input type="text" class="playlist-title" v-model="playlist.title" placeholder="Playlist name">
             <div v-for="item in playlist.tracks">
                 <input type="checkbox" v-model="item.check">
                 {{ item.title }} by {{ item.artist }}
+            </div>
+        </div>
+    </div>
+
+    <div ref="image-modal" class="hidden" @click="toggleImageModal">
+        <div class="modal-content" v-on:click.stop>
+            <div class="top-bar">
+                <h3>Select Playlist Image</h3>
+                <div class="button-base" style="margin-left: auto" @click="toggleImageModal">
+                    <svg xmlns="http://www.w3.org/2000/svg" width="32" height="32" fill="currentColor"
+                        viewBox="0 0 16 16">
+                        <path
+                            d="M4.646 4.646a.5.5 0 0 1 .708 0L8 7.293l2.646-2.647a.5.5 0 0 1 .708.708L8.707 8l2.647 2.646a.5.5 0 0 1-.708.708L8 8.707l-2.646 2.647a.5.5 0 0 1-.708-.708L7.293 8 4.646 5.354a.5.5 0 0 1 0-.708z" />
+                    </svg>
+                </div>
+            </div>
+            <div class="image-button-wrapper">
+                <input type="file" ref="imageSelector" @change="selectedButton">
+                <div class="button-base" @click="getRandomImage">Retrieve random image</div>
+            </div>
+            <div class="image-wrapper">
+                <img class="cover-image" :src="imageBuffer" v-if="imageBuffer">
             </div>
         </div>
     </div>
@@ -25,13 +74,11 @@ export default {
     data() {
         return {
             access_token: null,
-            playlist: {
-                title: '',
-                tracks: []
-            },
+            playlist: null,
             fetching: false,
             posting: false,
-            imageBuffer: null
+            imageBuffer: null,
+            user: null
         }
     },
     created() {
@@ -45,6 +92,21 @@ export default {
         } else {
             if (this.$cookies.get('access_token') !== null) {
                 this.access_token = this.$cookies.get('access_token')
+                fetch('https://api.spotify.com/v1/me', {
+                    headers: {
+                        'Authorization': 'Bearer ' + this.access_token
+                    }
+                }).then(res => {
+                    console.log(res.status);
+                    if (res.status !== 200) {
+                        this.$cookies.remove('access_token')
+                        location.href = "https://lumine.oryfox.de/"
+                    } else {
+                        res.json().then(data => {
+                            this.user = data
+                        })
+                    }
+                })
             } else {
                 console.log(location.hash);
                 const params = {}
@@ -76,6 +138,7 @@ export default {
         async fetchAndParsePlaylist() {
             let url = this.$refs.url.value
             this.fetching = true
+            this.playlist = {}
             var tracks = [];
 
             let res = await fetch(`https://lumine.oryfox.de/api?url=${encodeURIComponent(url)}`)
@@ -113,7 +176,7 @@ export default {
             })
             user = await user.json()
             let userId = user.id;
-        
+
             //Search tracks
             let tracks = []
             for (let i = 1; i < array.length; i++) {
@@ -141,7 +204,7 @@ export default {
                 })
             })
             playlist = await playlist.json()
-        
+
             //Add tracks to playlist
             while (tracks.length) {
                 await fetch(base + "/v1/playlists/" + playlist.id + "/tracks", {
@@ -157,23 +220,23 @@ export default {
             }
 
             if (this.imageBuffer) {
-                await fetch(base + "/v1/playlists/" + playlist.id + "/images", {
+                const mimeType = this.imageBuffer.split(',')[0].split(':')[1].split(';')[0]
+                console.log(mimeType);
+                let res = await fetch(base + "/v1/playlists/" + playlist.id + "/images", {
                     method: 'PUT',
                     headers: {
                         'Authorization': 'Bearer ' + this.access_token,
-                        'Content-Type': 'image/jpeg'
+                        'Content-Type': mimeType
                     },
                     body: this.imageBuffer.replace(/^data:image\/(png|jpg|jpeg);base64,/, "")
                 })
+                console.log(await res.text());
             }
 
             //Allow button to be clicked again
             this.posting = false
             //Reset playlist
-            this.playlist = {
-                title: '',
-                tracks: []
-            }
+            this.playlist = null
             this.imageBuffer = null
         },
         async selectedButton() {
@@ -212,7 +275,13 @@ export default {
                 this.imageBuffer = dataURL
             }
             img.src = url;
-        }
+        },
+        async toggleImageModal() {
+            this.$refs["image-modal"].classList.toggle("imageModal");
+        },
+        async openUserProfile() {
+            window.open("https://open.spotify.com/user/" + this.user.id, "_blank")
+        },
     }
 }
 </script>
@@ -249,11 +318,24 @@ export default {
     transition-duration: 0.3s;
 }
 
-input[type="file"] {
-    display: none;
+input[type="file"]::-webkit-file-upload-button {
+    border-radius: 0.5rem;
+    transition-duration: 0.3s;
+    padding: 0.4rem;
+    cursor: pointer;
+    display: flex;
+    align-items: center;
+    color: var(--color-text);
+    background: none;
+    border: none;
 }
 
-input[type="text"] {
+input[type="file"]::-webkit-file-upload-button:hover {
+    background: var(--color-background-soft);
+    transition-duration: 0.3s;
+}
+
+.url-input {
     width: 200px;
     padding: 0.5rem;
     border: thin solid var(--color-border);
@@ -261,9 +343,13 @@ input[type="text"] {
     background-color: var(--color-background-mute);
     color: var(--color-text);
     caret-color: var(--color-text);
+    height: 45px;
+    font-size: 0.9rem;
+    flex: 1;
 }
 
-input[type="text"]:focus {
+.playlist-title:focus,
+.url-input:focus {
     outline: none !important;
     border: thin solid var(--color-border) !important;
     background-color: var(--color-background-mute) !important;
@@ -284,16 +370,123 @@ input[type="text"]:focus {
     gap: 0.5rem;
 }
 
-img {
-    width: 100px;
-    height: 100px;
+.image-wrapper {
+    display: flex;
+    overflow: hidden;
+    border-radius: 0.5rem;
+    width: 300px;
+    height: 300px;
+    justify-content: center;
+    align-items: center;
+    background-color: var(--color-background-soft);
+}
+
+.cover-image {
+    height: 100%;
     border-radius: 0.5rem;
     border: solid thin var(--color-border);
 }
 
+.user-button {
+    display: flex;
+    flex-direction: row;
+    align-items: center;
+    justify-content: flex-start;
+    gap: 0.5rem;
+}
+
+.user-image {
+    height: 45px;
+    width: auto;
+    border-radius: 50%;
+    transform: scale(1);
+    transition-duration: 300ms;
+}
+
+.user-name {
+    font-weight: 600;
+}
+
+.user-image:hover {
+    transform: scale(1.1);
+    transition-duration: 300ms;
+}
+
 .playlist-title {
+    padding: 0.5rem;
+    border: thin solid var(--color-border);
+    border-radius: 0.5rem;
+    background-color: var(--color-background-mute);
+    color: var(--color-text);
+    caret-color: var(--color-text);
+    height: 45px;
+
     font-size: x-large;
     font-weight: 500;
     width: 100% !important;
+}
+
+.hidden {
+    display: none;
+}
+
+.imageModal {
+    display: flex;
+    position: fixed;
+    top: 0;
+    left: 0;
+    right: 0;
+    bottom: 0;
+    backdrop-filter: blur(10px) brightness(1.3);
+    -webkit-backdrop-filter: blur(10px) brightness(1.3);
+
+    justify-content: center;
+    padding-top: 10%;
+}
+
+.modal-content {
+    background-color: var(--color-background-mute);
+    position: relative;
+    width: 768px;
+    border-radius: 0.5rem;
+    border: solid thin var(--color-border);
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    padding-bottom: 2rem;
+    margin-bottom: auto;
+}
+
+.modal-content .top-bar {
+    background-color: var(--color-background-soft);
+    display: flex;
+    flex-direction: row;
+    align-items: center;
+    padding: 0.5rem;
+    border-bottom: solid thin var(--color-border);
+    border-top-left-radius: 0.5rem;
+    border-top-right-radius: 0.5rem;
+    width: 100%;
+}
+
+.image-button-wrapper {
+    display: flex;
+    flex-direction: row;
+    flex-wrap: wrap;
+    align-items: center;
+    justify-content: center;
+    gap: 0.5rem;
+    width: 100%;
+    min-height: 3rem;
+    padding: 0 1rem;
+}
+
+.image-button-wrapper .button-base:hover {
+    background: var(--color-background-soft);
+    transition-duration: 0.3s;
+}
+
+.image-button-wrapper input[type="file"] {
+    flex: 1;
 }
 </style>
